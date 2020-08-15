c9345bf1b265c0e0c9f6257ab79637da9e8ac645
shadoobie <chris.santen@gmail.com>
2020-08-15 03:04:40 -0600

    do'n the infura tutorial

--- infur/infur.py
+++ infur/infur.py
@@ -1,4 +1,5 @@
 import unittest
+import json
 from web3 import Web3
 
 class TestInfer(unittest.TestCase):
@@ -11,6 +12,16 @@ class TestInfer(unittest.TestCase):
         balance = web3.eth.getBalance("0x43778119cC6067C942d0E0729128d8691343339F")
         print("get balance: " + str(balance))
         print(web3.fromWei(balance, "ether"))
+
+    def test_smart_contract(self):
+        infur_url = "https://mainnet.infura.io/v3/302093503f54409ab515af1c1b775a6c"
+        web3 = Web3(Web3.HTTPProvider(infur_url))
+        self.assertTrue(web3.isConnected(), "web3 isn't connected.")
+        print("web3 is connected to " + infur_url)
+
+        abi = ""
+        address = "0xba11d00c5f74255f56a5e366f4f77f5a186d7f55"
+
 
 
 
