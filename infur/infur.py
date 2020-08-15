747f8b8fc88e08a0fb2999e365b3595be8dba673
shadoobie <chris.santen@gmail.com>
2020-08-15 02:34:13 -0600

    do'n the infura tutorial

--- infur/infur.py
+++ infur/infur.py
@@ -6,8 +6,11 @@ class TestInfer(unittest.TestCase):
         infur_url = "https://mainnet.infura.io/v3/302093503f54409ab515af1c1b775a6c"
         web3 = Web3(Web3.HTTPProvider(infur_url))
         self.assertTrue(web3.isConnected(), "web3 isn't connected.")
-        web3.eth.blockNumber
-        print("get balance: " + str(web3.eth.getBalance("0x43778119cC6067C942d0E0729128d8691343339F")))
+        print("web3 is connected to " + infur_url)
+        print("current block number: " + str(web3.eth.blockNumber))
+        balance = web3.eth.getBalance("0x43778119cC6067C942d0E0729128d8691343339F")
+        print("get balance: " + str(balance))
+        print(web3.fromWei(balance, "ether"))
 
 
 
